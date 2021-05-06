cb8535663aa3ab48dc85f4d363deb1c5eb34d734
jéssica senger <jessicasbogoni1@gmail.com>
2021-05-06 14:47:40 -0400

    Create RepositoryList component

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,30 @@
+const repositoryName = 'unform';
+
+export function RepositoryList() {
+  return (
+    <section className="respository-list">
+      <h1>Lista de repositórios</h1>
+
+      <ul>
+        <li>
+          <strong>{ repositoryName }</strong>
+          <p>Forms in React</p>
+
+          <a href="">Acessar repositório</a>
+        </li>
+        <li>
+          <strong>{ repositoryName }</strong>
+          <p>Forms in React</p>
+
+          <a href="">Acessar repositório</a>
+        </li>
+        <li>
+          <strong>{ repositoryName }</strong>
+          <p>Forms in React</p>
+
+          <a href="">Acessar repositório</a>
+        </li>
+      </ul>
+    </section>
+  );
+}
